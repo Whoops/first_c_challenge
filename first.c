ec46abe212e7dce8ec3beed7d53908f62f330747
Walton Hoops <me@waltonhoops.com>
2011-05-03 13:46:56 -0400

    cleanup spacing a little

--- first.c
+++ first.c
@@ -14,37 +14,45 @@ static record records[MAX_REC];
 void echo_char(c) {
 	printf("You entered %c\n",c);
 }
+
 void print_prompt() {
 	printf("Enter Character : . to quit\n");
 }
+
 char read_char() {
 	char c;
 	scanf("%c",&c);
 	return c;
 }
+
 void eat_extra_input() {
 	while (getchar() != '\n'); /* discard any other input upto newline */
 }
+
 void record_init() {
 	int i;
 	for(i = 0; i < 26; i++) {
 		records[i].key   = (97+i);
 		records[i].value = 0;
 	}
+
 	for(i = 26; i < MAX_REC; i++) {
 		records[i].key   = (22+i);
 		records[i].value = 0;
 	}
 }
+
 int char_hash(char c) {
 	assert(((c > 47) && (c < 58)) || ((c > 96) && (c < 123)));
 	return (c > 96 ? c - 97 : c - 22);
 }
+
 void record_char(char c) {
 	int index;
 	index = char_hash(c);
 	records[index].value = records[index].value++;
 }
+
 void record_print() {
 	int i = 0;
 	printf("Records\n");	
@@ -53,6 +61,7 @@ void record_print() {
 		printf("%3c %5d\n",records[i].key,records[i].value);
 	}	
 }
+
 int main(int argc, char **argv) {
 	char c = 0;
 	record_init();
